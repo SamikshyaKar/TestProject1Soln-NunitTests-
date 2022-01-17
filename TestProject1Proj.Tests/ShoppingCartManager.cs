5db2aea18080963481bfbfd753e3f727b4c1fad3
samikshya.sibani <samikshya.sibani@gmail.com>
2022-01-17 20:47:04 +0530

    -- next commit

--- TestProject1Proj.Tests/ShoppingCartManager.cs
+++ TestProject1Proj.Tests/ShoppingCartManager.cs
@@ -1,19 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace TestProject1Proj
 {
     internal class ShoppingCartManager
     {
+
+        private List<addtocarditems> _shoppingcartItems;
         public ShoppingCartManager()
         {
+            _shoppingcartItems = new List<addtocarditems>();
+
         }
 
         internal AddToCartResponse addtocart(addtoCartRequest request)
         {
+            _shoppingcartItems.Add(request.item);
+
             return new AddToCartResponse()
             {
 
-                Items = new addtocarditems[] { request.item }
+                //Items = new addtocarditems[] { request.item }
+
+                Items = _shoppingcartItems.ToArray()
             };
         }
     }
--- TestProject1Proj.Tests/shoppingCartTests.cs
+++ TestProject1Proj.Tests/shoppingCartTests.cs
@@ -35,8 +35,82 @@ namespace TestProject1Proj
 
         }
 
+        [Test]
+
+        public void shouldreturnarticlseaddedtocart()
+        {
+            var item1 = new addtocarditems()
+            {
+                ArticleID = 45,
+                Quantity = 5
+            };
 
 
-    
+            var request = new addtoCartRequest()
+            {
+                item = item1
+            };
+
+            var manager = new ShoppingCartManager();
+            AddToCartResponse response = manager.addtocart(request);
+
+            var item2 = new addtocarditems()
+            {
+                ArticleID = 48,
+                Quantity = 5
+            };
+
+
+             request = new addtoCartRequest()
+            {
+                item = item2
+            };
+            response = manager.addtocart(request);
+
+            Assert.NotNull(response);
+            Assert.Contains(item1, response.Items);
+            Assert.Contains(item2, response.Items);
+
+        }
+
+        //public void shouldreturnarticlesaadedtocars()
+        //{
+
+        //    var item1 = new addtocarditems
+        //    {
+        //        ArticleID = 45,
+        //        Quantity = 10
+        //    };
+
+        //    var item2 = new addtocarditems
+        //    {
+        //        ArticleID = 46,
+        //        Quantity = 20
+        //    };
+
+        //    var request = new addtoCartRequest()
+        //    {
+        //        item = item1
+
+        //    };
+
+        //    request = new addtoCartRequest()
+        //    {
+        //        item = item2
+        //    };
+
+        //    var manager = new ShoppingCartManager();
+        //    AddToCartResponse response = manager.addtocart( request);
+        //    response = manager.addtocart(request);
+
+        //    Assert.IsNull(response);
+        //    Assert.Contains(item1, response.Items);
+        //    Assert.Contains(item2, response.Items);
+
+
+
+        //}
+
+
     }
 }
